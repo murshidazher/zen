4aa7d95bec208306535ccd86988c0594c21bd568
murshidazher <hello@murshidazher.com>
2023-08-21 05:39:52 +0530

    feat: add fallback avatar

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,7 +1,8 @@
 import { siteConfig } from "@/config/site";
 import { googleSansDisplay } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import Image from "@/components/ui/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Icons } from "@/components/ui/icons";
 import { Indicator } from "@/components/ui/indicator";
 
 export const Landing = ({
@@ -17,14 +18,20 @@ export const Landing = ({
               <div className="flex items-center space-x-5">
                 <div className="shrink-0">
                   <div className="relative">
-                    <Image
-                      src={"/images/avatar.jpg"}
-                      width={64}
-                      height={64}
-                      alt={`A photo of ${siteConfig.name}.`}
-                      className="h-16 w-16 rounded-full border dark:border-2 dark:border-zinc-800 lg:h-24 lg:w-24"
-                      priority
-                    />
+                    <Avatar className="h-24 w-24">
+                      <AvatarImage
+                        height={64}
+                        width={64}
+                        src={"/images/avatar.jpg"}
+                        className="rounded-full border dark:border-2 dark:border-zinc-800"
+                        alt={`A photo of ${siteConfig.name}.`}
+                      />
+                      <AvatarFallback>
+                        <div>
+                          <Icons.logo className="h-16 w-16" />
+                        </div>
+                      </AvatarFallback>
+                    </Avatar>
                     <span
                       className="absolute inset-0 rounded-full shadow-inner"
                       aria-hidden="true"
